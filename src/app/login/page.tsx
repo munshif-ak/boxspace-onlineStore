1a41163a43ba554017fdfc0b656453651022ce15
Mohammed Munshif <munshif.upcode@gmail.com>
2024-03-24 01:08:02 +0530

    completed fetch login api

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,35 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/loginScreen.css";
 import { Button, TextField } from "@mui/material";
+import { json } from "stream/consumers";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const handleEmailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: any) => {
+    setPassword(event?.target.value);
+  };
+
+   async function clickLogin() {
+    const response = await fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: email,
+        password: Password,
+      }),
+    });
+    const data = await response.json();
+    console.log('@@@@',data)
+  };
+
   return (
     <div className="login-maicontainer">
       <h1 style={{ fontWeight: "700" }}>Login</h1>
@@ -22,14 +48,21 @@ function Login() {
           id="outlined-basic"
           label="Email adress"
           variant="outlined"
+          onChange={handleEmailChange}
         />
         <TextField
           className="login-textField"
           id="outlined-basic"
           label="Password"
           variant="outlined"
+          onChange={handlePasswordChange}
         />
-        <Button className="login-button" variant="contained" style={{ backgroundColor: 'transparent' }}>
+        <Button
+        onClick={clickLogin}
+          className="login-button"
+          variant="contained"
+          style={{ backgroundColor: "transparent" }}
+        >
           Login
         </Button>
       </div>
